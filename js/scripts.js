b33bc72afe67568ac9b65d8aa5f9b15d0320123c
sonhappy2 <ssiw3604@gamil.com>
2021-05-06 13:24:00 +0900

    테마

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Resume v6.0.3 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - Freelancer v6.0.6 (https://startbootstrap.com/theme/freelancer)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 (function ($) {
     "use strict"; // Start of use strict
@@ -20,7 +20,7 @@
             if (target.length) {
                 anime({
                     targets: 'html, body',
-                    scrollTop: target.offset().top,
+                    scrollTop: target.offset().top - 72,
                     duration: 1000,
                     easing: 'easeInOutExpo'
                 });
@@ -29,14 +29,49 @@
         }
     });
 
+    // Scroll to top button appear
+    $(document).scroll(function () {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
     // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").on('click', function () {
-        $(".navbar-collapse").collapse("hide");
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
     });
 
     // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav"
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Floating label headings for the contact form
+    $(function () {
+        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function () {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function () {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
     });
 
 })(jQuery); // End of use strict
